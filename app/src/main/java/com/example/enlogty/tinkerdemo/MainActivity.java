f4ba394933311e1e55ac2132656d310a4ce451f6
Gityuan <gityuan@google.com>
2018-01-04 14:11:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/enlogty/tinkerdemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.enlogty.tinkerdemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tinkerpatch.sdk.TinkerPatch;
+
+public class MainActivity extends AppCompatActivity {
+    private Button request_patch;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
+        request_patch = findViewById(R.id.request_patch);
+        request_patch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed_7zip");
+
+                //强制访问更新patch
+                TinkerPatch.with().fetchPatchUpdate(true);
+            }
+        });
+    }
+}
